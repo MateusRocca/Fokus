4145462a989ad7789cbec324dfdadb082bbfcd8d
Mateus Rocca <galdinorocca@outlook.com>
2024-03-27 12:01:34 -0300

    Adição de novas funcionalidades no formulário

--- script-crud.js
+++ script-crud.js
@@ -2,13 +2,23 @@ const botaoAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formularioTarefa = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
+const btCancelarTarefa = document.querySelector('.app__form-footer__button--cancel');
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+function atualizarTarefas() {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
+function cancelarTarefa() {
+    textArea.value = '';
+    formularioTarefa.classList.add('hidden');
+}
 
 function criarElementoTarefas(tarefa) {
     const li = document.createElement('li');
     li.classList.add('app__section-task-list-item');
-
+ 
     const svg = document.createElement('svg');
     svg.innerHTML = `
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -23,6 +33,14 @@ function criarElementoTarefas(tarefa) {
 
     const botao = document.createElement('button');
     botao.classList.add('app_button-edit');
+    botao.onclick = () => {
+        let novaDescricao = prompt('Qual a nova tarefa?');
+        if(novaDescricao){
+            paragrafo.textContent = novaDescricao;
+            tarefa.descricao = novaDescricao;
+            atualizarTarefas();
+        }
+    }
 
     const imagemBotao = document.createElement('img');
     imagemBotao.setAttribute('src', './imagens/edit.png');
@@ -39,6 +57,8 @@ botaoAdicionarTarefa.addEventListener('click', () => {
     formularioTarefa.classList.toggle('hidden');
 })
 
+btCancelarTarefa.addEventListener('click', cancelarTarefa);
+
 formularioTarefa.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -47,7 +67,11 @@ formularioTarefa.addEventListener('submit', (e) => {
     };
 
     tarefas.push(tarefa);
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    const elementoTarefa = criarElementoTarefas(tarefa);
+    ulTarefas.append(elementoTarefa);
+    atualizarTarefas();
+    textArea.value = '';
+    formularioTarefa.classList.add('hidden');
 })
 
 tarefas.forEach(tarefa => {
